0845324c980a28be4602aa1da7d6d3e17d15d2e9
Christian Schou <christian.schou@hotmail.com>
2023-06-17 10:46:18 +0200

    Add DTOs

---
+++ src/stripe.shared/DTOs/Stripe/Payments/StripeCardDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace stripe.shared.DTOs.Stripe.Payments
+{
+    public record StripeCardDto(
+        string Name,
+        string CardNumber,
+        string ExpirationYear,
+        string ExpirationMonth,
+        string Cvc);
+}
---
+++ src/stripe.shared/DTOs/Stripe/Payments/StripePaymentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace stripe.shared.DTOs.Stripe.Payments
+{
+    public record StripePaymentDto(
+        string CustomerId,
+        string ReceiptEmail,
+        string Description,
+        string Currency,
+        long Amount);
+}
+
