c14b34275d9f2958545d48d341b7d1da5e0ea597
Christian Schou <christian.schou@hotmail.com>
2023-06-17 10:43:04 +0200

    Remove old API project

--- Models/Stripe/AddStripeCustomer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Stripe_Payments_Web_Api.Models.Stripe
-{
-	public record AddStripeCustomer(
-		string Email,
-		string Name,
-		AddStripeCard CreditCard);
-}
-
--- Models/Stripe/StripeCustomer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace Stripe_Payments_Web_Api.Models.Stripe
-{
-	public record StripeCustomer(
-		string Name,
-		string Email,
-		string CustomerId);
-}
-
