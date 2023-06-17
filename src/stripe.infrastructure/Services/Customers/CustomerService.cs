647cc9e75e8fa579d117ddd298542f7a7c747024
Christian Schou <christian.schou@hotmail.com>
2023-06-17 10:44:06 +0200

    Add Customer Service
    Initial add. Needs implementation of logic

---
+++ src/stripe.infrastructure/Services/Customers/CustomerService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using stripe.application.Services.Customers;
+using stripe.domain.Models.Customers;
+
+namespace stripe.infrastructure.Services.Customers
+{
+    public class CustomerService : ICustomerService
+    {
+        public CustomerService()
+        {
+        }
+
+        public Task<Customer> CreateCustomerAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteCustomerAsync(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerByIdAsync(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> GetCustomerByStripeIdAsync(string stripeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Customer> UpdateCustomerAsync(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
