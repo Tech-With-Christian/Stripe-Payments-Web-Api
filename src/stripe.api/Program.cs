5f73f62084d730823af518cd684b60c8cef3389a
Christian Schou <christian.schou@hotmail.com>
2023-06-17 12:52:18 +0200

    Add Static Logger

--- src/stripe.api/Program.cs
+++ src/stripe.api/Program.cs
@@ -1,27 +1,43 @@
-﻿using stripe.infrastructure;
+﻿using Serilog;
+using stripe.infrastructure;
+using stripe.infrastructure.Services.Common;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddInfrastructure(builder.Configuration);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+StaticLogger.EnsureInitialized();
+Log.Information("Stripe API is booting up...");
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddInfrastructure(builder.Configuration);
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
+
 }
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex) when (!ex.GetType().Name.Equals("HostAbortedException", StringComparison.Ordinal))
+{
+    StaticLogger.EnsureInitialized();
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    StaticLogger.EnsureInitialized();
+    Log.Information("Server Shutting down...");
+    Log.CloseAndFlush();
+}
 
---
+++ src/stripe.infrastructure/Services/Common/StaticLogger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Serilog;
+
+namespace stripe.infrastructure.Services.Common
+{
+    public static class StaticLogger
+    {
+        /// <summary>
+        /// Ensures that the logger is initialized.
+        /// </summary>
+        public static void EnsureInitialized()
+        {
+            if (Log.Logger is not Serilog.Core.Logger)
+            {
+                Log.Logger = new LoggerConfiguration()
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console()
+                    .CreateLogger();
+            }
+        }
+    }
+}
+
