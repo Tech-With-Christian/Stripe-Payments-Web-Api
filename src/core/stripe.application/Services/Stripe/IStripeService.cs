6152b44af48762be080c660d8fab78717e7d9bb6
Christian Schou <christian.schou@hotmail.com>
2023-06-17 10:46:04 +0200

    Add Stripe Service
    Moved from original API and implements new domain models.

---
+++ src/core/stripe.application/Services/Stripe/IStripeService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using stripe.application.Services.Common;
+using stripe.domain.Models.Stripe.Customers;
+using stripe.domain.Models.Stripe.Payments;
+
+namespace stripe.application.Services.Stripe
+{
+    public interface IStripeService : IScopedService
+    {
+        Task<string> CreateStripeCustomerAsync(StripeCustomer customer, CancellationToken ct);
+        Task<string> CreateStripePaymentAsync(StripePayment payment, CancellationToken ct);
+    }
+}
+
