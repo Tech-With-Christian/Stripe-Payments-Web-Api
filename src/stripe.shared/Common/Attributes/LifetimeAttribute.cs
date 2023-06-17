3cd75330a0d32d25b214b2db7275597c75d2cf0f
Christian Schou <christian.schou@hotmail.com>
2023-06-17 13:02:37 +0200

    Add Lifetime Attribute

---
+++ src/stripe.shared/Common/Attributes/LifetimeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace stripe.shared.Common.Attributes
+{
+    /// <summary>
+    /// Specifies the service lifetime for a class.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class LifetimeAttribute : Attribute
+    {
+        /// <summary>
+        /// Gets the service lifetime associated with the class.
+        /// </summary>
+        public ServiceLifetime Lifetime { get; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LifetimeAttribute"/> class with the specified service lifetime.
+        /// </summary>
+        /// <param name="lifetime">The service lifetime to associate with the class.</param>
+        public LifetimeAttribute(ServiceLifetime lifetime)
+        {
+            Lifetime = lifetime;
+        }
+    }
+}
+
