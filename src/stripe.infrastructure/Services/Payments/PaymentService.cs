9da3cf4b56fd6b48dcc6959e59a0f8ba873b1656
Christian Schou <christian.schou@hotmail.com>
2023-06-17 10:45:01 +0200

    Add Payment Service
    Add Payment service for retrieval of payments made at Stripe, but locally. TODO: add implementation.

---
+++ src/stripe.infrastructure/Services/Payments/PaymentService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using stripe.application.Services.Payments;
+using stripe.domain.Models.Payments;
+
+namespace stripe.infrastructure.Services.Payments
+{
+    public class PaymentService : IPaymentService
+    {
+        public PaymentService()
+        {
+        }
+
+        public Task<Payment> GetPaymentByIdAsync(Guid paymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Payment> GetPaymentByStripeIdAsync(string stripePaymentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Payment>> GetPaymentsByCustomerId(Guid customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Payment>> GetPaymentsByStripeCustomerId(string stripeCutomerId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
